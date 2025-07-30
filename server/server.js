761ca3ca25b41a78060b5e26f3e0301fcb3023fd
XTheSabreGamerX <jhaycee081602@gmail.com>
2025-07-30 19:51:51 +0800

    Adjusted Server file - Changed line of code in server.js

--- server/server.js
+++ server/server.js
@@ -45,6 +45,6 @@ app.get('/', (req, res) => {
   res.send('Hunny’s Crémerie Server is running!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
