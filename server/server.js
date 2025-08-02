f472646c5c6ecb250a2fc7a4b69fc2290092e559
Juliane Lagrimas <uyan@Julianes-MacBook-Air.local>
2025-08-02 09:46:01 +0800

    Fix Development Files

--- server/server.js
+++ server/server.js
@@ -14,8 +14,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/request', requestRoutes);
