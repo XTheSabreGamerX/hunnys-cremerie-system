8cc3a1e153d193566aa7c8e7649cec0b6f7f3f4e
XTheSabreGamerX <jhaycee081602@gmail.com>
2025-10-23 22:04:31 +0800

    Update server.js - Added development environment

--- server/server.js
+++ server/server.js
@@ -1,99 +1,115 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cron = require('node-cron');
-const authRoutes = require('./routes/auth');
-const requestRoutes = require('./routes/request');
-const dashboardRoutes = require('./routes/dashboard');
-const userRoutes = require('./routes/user');
-const resetRequestRoutes = require('./routes/resetRequest');
-const activityRoutes = require('./routes/activityLog');
-const inventoryRoutes = require('./routes/inventory');
-const saleRoutes = require('./routes/sale');
-const salesReportRoutes = require('./routes/salesReport');
-const supplierRoutes = require('./routes/supplier');
-const customerRoutes = require('./routes/customer');
-const notificationRoutes = require('./routes/notification');
-const actionRequestRoutes = require('./routes/actionRequest');
-const cakeRoutes = require('./routes/cake');
-const backupRoutes = require('./routes/backup');
-const settingsRoutes = require('./routes/settings');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cron = require("node-cron");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth");
+const requestRoutes = require("./routes/request");
+const dashboardRoutes = require("./routes/dashboard");
+const userRoutes = require("./routes/user");
+const resetRequestRoutes = require("./routes/resetRequest");
+const activityRoutes = require("./routes/activityLog");
+const inventoryRoutes = require("./routes/inventory");
+const saleRoutes = require("./routes/sale");
+const salesReportRoutes = require("./routes/salesReport");
+const supplierRoutes = require("./routes/supplier");
+const customerRoutes = require("./routes/customer");
+const notificationRoutes = require("./routes/notification");
+const actionRequestRoutes = require("./routes/actionRequest");
+const cakeRoutes = require("./routes/cake");
+const backupRoutes = require("./routes/backup");
+const settingsRoutes = require("./routes/settings");
 const app = express();
 
-const { batchUpdateStatuses } = require('./controllers/inventoryController');
+const { batchUpdateStatuses } = require("./controllers/inventoryController");
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI =
+  process.env.NODE_ENV === "development"
+    ? process.env.MONGO_URI_DEV
+    : process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  console.error("MongoDB URI is not defined. Please check your .env file.");
+  process.exit(1);
+}
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    const dbEnv =
+      process.env.NODE_ENV === "development" ? "Development" : "Production";
+    console.log(`MongoDB connected! (${dbEnv})`);
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1);
+  });
 
 const allowedOrigins = [
-  'http://localhost:3000',
-  'https://www.hunnyscremerie.online',
-  'https://hunnys.netlify.app'
+  "http://localhost:3000",
+  "https://www.hunnyscremerie.online",
+  "https://hunnys.netlify.app",
+  "https://hunnysdev.netlify.app",
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  exposedHeaders: ['Content-Disposition'],
-}));
-
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    exposedHeaders: ["Content-Disposition"],
+  })
+);
 
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/request', requestRoutes);
-app.use('/api/dashboard', dashboardRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/resetRequest', resetRequestRoutes);
-app.use('/api/activitylog', activityRoutes)
-app.use('/api/inventory', inventoryRoutes);
-app.use('/api/sales', saleRoutes);
-app.use('/api/salesReport', salesReportRoutes);
-app.use('/api/suppliers', supplierRoutes);
-app.use('/api/customers', customerRoutes);
-app.use('/api/notifications', notificationRoutes);
-app.use('/api/actionRequest', actionRequestRoutes);
-app.use('/api/cake', cakeRoutes);
-app.use('/api/backup', backupRoutes);
-app.use('/api/settings', settingsRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/request", requestRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/resetRequest", resetRequestRoutes);
+app.use("/api/activitylog", activityRoutes);
+app.use("/api/inventory", inventoryRoutes);
+app.use("/api/sales", saleRoutes);
+app.use("/api/salesReport", salesReportRoutes);
+app.use("/api/suppliers", supplierRoutes);
+app.use("/api/customers", customerRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/actionRequest", actionRequestRoutes);
+app.use("/api/cake", cakeRoutes);
+app.use("/api/backup", backupRoutes);
+app.use("/api/settings", settingsRoutes);
 
 // Cron scheduler. Runs every 15 minutes to update inventory item status.
-cron.schedule('*/15 * * * *', async () => {
-  console.log('Running inventory status batch update...');
+cron.schedule("*/15 * * * *", async () => {
+  console.log("Running inventory status batch update...");
   try {
     await batchUpdateStatuses();
-    console.log('Inventory status updated successfully!');
+    console.log("Inventory status updated successfully!");
   } catch (error) {
-    console.error('Error updating inventory status: An error occurred.');
+    console.error("Error updating inventory status: An error occurred.");
   }
 });
 
 if (!MONGO_URI) {
-  console.error('MONGO_URI is not defined. Please check your .env file.');
+  console.error("MONGO_URI is not defined. Please check your .env file.");
   process.exit(1);
 }
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected!'))
-.catch((err) => {
-  console.error('MongoDB connection failed:', err.message);
-  process.exit(1);
+app.get("/", (req, res) => {
+  res.send("Hunny’s Crémerie Server is running!");
 });
 
-app.get('/', (req, res) => {
-  res.send('Hunny’s Crémerie Server is running!');
-});
-
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   //console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
